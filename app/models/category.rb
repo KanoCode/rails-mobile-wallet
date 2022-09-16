3c84ede2ddf912aec26afdbcae46221f4a0934e2
KanoCode <kanokanombola@gmail.com>
2022-09-16 02:22:15 +0200

    Update routes, Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ApplicationRecord
   belongs_to :user
   has_many :transactions
   has_many :payments, through: :transactions
+  has_one_attached :icon
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :category 
+  resources :transaction
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "category#index"
 end
